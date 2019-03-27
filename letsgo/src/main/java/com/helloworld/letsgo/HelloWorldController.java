57743c5187c4ab7d2225c3e124d3d221d7d0758c
Q10Viking <1193094618@qq.com>
2019-03-28 03:19:38 +0800

    create first letsgo project for start

---
+++ letsgo/src/main/java/com/helloworld/letsgo/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.helloworld.letsgo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloWorldController{
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+    
+}
